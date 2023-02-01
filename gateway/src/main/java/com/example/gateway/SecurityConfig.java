f50a68f9f915496e31f097ad9998fadd25ec02fa
Giovanni Piccolo <piccolog93@gmail.com>
2023-02-01 10:05:51 +0100

    add oauth2 client and oauth2 resource server dependencies and update configurations

--- gateway/src/main/java/com/example/gateway/SecurityConfig.java
+++ gateway/src/main/java/com/example/gateway/SecurityConfig.java
@@ -2,9 +2,11 @@ package com.example.gateway;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestCustomizers;
+import org.springframework.security.oauth2.client.web.server.DefaultServerOAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 
@@ -13,7 +15,10 @@ import org.springframework.security.web.server.csrf.CookieServerCsrfTokenReposit
 public class SecurityConfig {
 
     @Bean
-    SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception {
+    SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http, ReactiveClientRegistrationRepository clientRegistrationRepository) throws Exception {
+
+        DefaultServerOAuth2AuthorizationRequestResolver pkceResolver = new DefaultServerOAuth2AuthorizationRequestResolver(clientRegistrationRepository);
+        pkceResolver.setAuthorizationRequestCustomizer(OAuth2AuthorizationRequestCustomizers.withPkce());
 
         return http
                 .headers(headers -> headers.contentSecurityPolicy("script-src 'self'"))
@@ -21,7 +26,7 @@ public class SecurityConfig {
 //                        .pathMatchers("/**").permitAll()
                         .anyExchange().authenticated())
                 .csrf(csrf -> csrf.csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse()))
-                .formLogin(Customizer.withDefaults())
+                .oauth2Login(login -> login.authorizationRequestResolver(pkceResolver))
                 .build();
     }
 }
--- resource/src/main/java/com/example/resource/SecurityConfig.java
+++ resource/src/main/java/com/example/resource/SecurityConfig.java
@@ -10,10 +10,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final String jwksUri = "http://auth-server:9000/oauth2/jwks";
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http
+                .oauth2ResourceServer(r -> r.jwt().jwkSetUri(jwksUri))
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/**").permitAll()
                         .anyRequest().authenticated())
